213a1d562cae252c45f49bf2a5a13cdfa294f132
mazikwyry <a.mazur@exlabs.co.uk>
2017-03-21 07:27:02 +0100

    add basic auth

--- app.rb
+++ app.rb
@@ -12,7 +12,11 @@ class App < Sinatra::Base
     set :show_exceptions, :after_handler
   end
 
+  API_KEY = '8L9qh77q9570H0LIz90Aj00T5mcOHW1w'.freeze
+  API_SECRET = 'G52uFXHPjyxRY3JdBIsw562uJ8bUdrE2'.freeze
+
   post '/api/v1/project/:id/reports' do
+    protected!
     request.body.rewind
     Watchdocs::ReportGenerator.new(
       requests_json: request.body.read,
@@ -39,5 +43,20 @@ class App < Sinatra::Base
 
   error Watchdocs::InvalidJsonError do
     [400, {}, json(errors: env['sinatra.error'].message)]
+  end
+
+  helpers do
+    def protected!
+      return if authorized?
+      headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+      halt 401, "Not authorized\n"
+    end
+
+    def authorized?
+      #TODO: Authorize by project credentials
+      @auth ||= Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? && @auth.basic? && @auth.credentials &&
+        @auth.credentials == [API_KEY, API_SECRET]
+    end
   end
 end
